0427fc63aec9268e99ba8f013778dfab42dd1520
AdamBalski <adambalski1@gmail.com>
2020-08-04 19:08:39 +0200

    Initial commit

---
+++ src/test/java/com/sellit/sellit/SellItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellit.sellit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
