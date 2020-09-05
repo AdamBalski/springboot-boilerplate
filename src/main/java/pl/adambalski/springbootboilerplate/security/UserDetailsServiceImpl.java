58e69009d93abf79f0db300754547b4d71315649
AdamBalski <adambalski1@gmail.com>
2020-09-05 23:30:15 +0200

    ADDED: UserDetailsServiceImpl

--- src/main/java/pl/adambalski/springbootboilerplate/security/UserDetailsServiceImpl.java
+++ src/main/java/pl/adambalski/springbootboilerplate/security/UserDetailsServiceImpl.java
@@ -1,2 +1,25 @@
-package pl.adambalski.springbootboilerplate.security;public class UserDetailsServiceImpl {
+package pl.adambalski.springbootboilerplate.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import pl.adambalski.springbootboilerplate.repository.UserRepository;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.getByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"))
+                .toUserDetails();
+    }
 }
