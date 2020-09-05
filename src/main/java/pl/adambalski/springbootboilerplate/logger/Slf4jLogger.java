01346145f156c25e64d915491eb59b85867996cf
AdamBalski <adambalski1@gmail.com>
2020-09-05 23:45:13 +0200

    ADDED: Slf4Logger

---
+++ src/main/java/pl/adambalski/springbootboilerplate/logger/Slf4jLogger.java
@@ -0,0 +1,23 @@
+package pl.adambalski.springbootboilerplate.logger;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of {@link Logger}
+ *
+ * @see pl.adambalski.springbootboilerplate.logger.Logger
+ * @author Adam Balski
+ */
+public class Slf4jLogger implements Logger {
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void log(String string, Class source, Status status) {
+        org.slf4j.Logger logger = LoggerFactory.getLogger(source);
+
+        switch (status) {
+            case INFO -> logger.info(string);
+            case EXCEPTION -> logger.error(string);
+            case DEBUG -> logger.debug(string);
+        }
+    }
+}
