98beebb08274c82be3ebe35a9143e1ed549df9c1
AdamBalski <adambalski1@gmail.com>
2021-07-23 21:06:39 +0200

    ADDED: JavaDocs for RandomAlphaNumericStringGenerator

--- src/main/java/pl/adambalski/springbootboilerplate/util/RandomAlphaNumericStringGenerator.java
+++ src/main/java/pl/adambalski/springbootboilerplate/util/RandomAlphaNumericStringGenerator.java
@@ -4,6 +4,13 @@ import net.bytebuddy.utility.RandomString;
 import org.springframework.stereotype.Component;
 import pl.adambalski.springbootboilerplate.security.SecurityConfiguration;
 
+/**
+ * Is used to create random strings of specified length, which contain numbers and letters (both uppercase and lowercase). <br><br>
+ *
+ * @author Adam Balski
+ * @see SecurityConfiguration
+ * @see RandomString
+ */
 @Component
 public class RandomAlphaNumericStringGenerator {
     private final int length;
