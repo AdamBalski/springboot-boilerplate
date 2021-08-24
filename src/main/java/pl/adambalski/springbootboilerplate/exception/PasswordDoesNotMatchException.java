ac36373a28d5a4a08accdbcaa7a83f87d627e99b
AdamBalski <adambalski1@gmail.com>
2021-08-24 19:27:55 +0200

    ADDED: PasswordDoesNotMatchException

--- src/main/java/pl/adambalski/springbootboilerplate/exception/PasswordDoesNotMatchException.java
+++ src/main/java/pl/adambalski/springbootboilerplate/exception/PasswordDoesNotMatchException.java
@@ -1,2 +1,10 @@
-package pl.adambalski.springbootboilerplate.exception;public class PasswordDoesNotMatchException {
+package pl.adambalski.springbootboilerplate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class PasswordDoesNotMatchException extends ResponseStatusException {
+    public PasswordDoesNotMatchException() {
+        super(HttpStatus.UNAUTHORIZED, "PASSWORD_DOES_NOT_MATCH_EXCEPTION");
+    }
 }
--- src/test/java/pl/adambalski/springbootboilerplate/exception/PasswordDoesNotMatchExceptionTest.java
+++ src/test/java/pl/adambalski/springbootboilerplate/exception/PasswordDoesNotMatchExceptionTest.java
@@ -1,6 +1,20 @@
 package pl.adambalski.springbootboilerplate.exception;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordDoesNotMatchExceptionTest {
-  
+    @Test
+    void testConstructor() {
+        var passwordDoesNotMatchException = new PasswordDoesNotMatchException();
+        var responseStatusException = new ResponseStatusException(HttpStatus.UNAUTHORIZED, "PASSWORD_DOES_NOT_MATCH_EXCEPTION");
+
+        assertAll(
+                () -> assertEquals(responseStatusException.getReason(), passwordDoesNotMatchException.getReason()),
+                () -> assertEquals(responseStatusException.getStatus(), passwordDoesNotMatchException.getStatus())
+        );
+    }
 }
